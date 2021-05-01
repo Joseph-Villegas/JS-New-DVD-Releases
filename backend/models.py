40ce20454d854e31f81f4dfe3b799da136e2aa62
Joseph-Villegas <jovillegas@csumb.edu>
2021-04-30 23:46:27 -0700

    created Movie model

---
+++ backend/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import *
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(session_options={"autoflush": False})
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    id = Column('id', db.Integer, autoincrement=True, primary_key=True)
+    imdb_id = Column('imdb_id', db.String(255), nullable=False, unique=True)
+    tmdb_id = Column('tmdb_id', db.Integer, nullable=False, unique=True)
+    title = Column('title', db.String(255), nullable=False)
+    poster = Column('poster', db.String(255), nullable=False)
+    overview = Column('overview', db.Text, nullable=False)
+    rating = Column('rating', db.Float, nullable=False)
+
+    def __init__(self, imdb_id, tmdb_id, title, poster, overview, rating):
+        self.imdb_id = imdb_id
+        self.tmdb_id = tmdb_id
+        self.title = title
+        self.poster = f'https://image.tmdb.org/t/p/w342{poster}'
+        self.overview = overview
+        self.rating = rating
+    
+    def __repr__(self):
+        return '<Movie %r>' % self.title
+    
+    def __str__(self):
+        return f'{{ "id": "{self.id}", "imdb_id": "{self.imdb_id}", "tmdb_id": "{self.tmdb_id}", "title": "{self.title}", "poster": "{self.poster}", "overview": "{self.overview}", "rating": "{self.rating}" }}'
