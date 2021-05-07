e93df95712952403e06f7c22a848aba2df3cc537
Joseph-Villegas <jovillegas@csumb.edu>
2021-05-06 21:36:16 -0700

    added asynchronous function to scrape and add movies to db

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,10 @@
-from flask import Flask, jsonify
-from scraper import scrape, search_by_week
-from models import db, Movie
 import os
-import json
-
-# https://api.themoviedb.org/3/find/tt0076759?api_key={}&language=en-US&external_source=imdb_id
-# import requests
-
-# x = requests.get('https://w3schools.com')
-# print(x.status_code)
-
-# >>> payload = {'key1': 'value1', 'key2': 'value2'}
-# >>> r = requests.get('https://httpbin.org/get', params=payload)
-
-# print(r.url)
-# r.text
-# r.encoding
-# r.json()
+import asyncio
+import aiohttp
+import requests
+from scraper import scrape
+from models import db, Movie
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -26,45 +14,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY', '')
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>DVD Release Dates API</h1> \
         <p>This API retrieves retail release schedules for DVDs by performing a web scrape</p> \
         <p>Source: https://www.dvdsreleasedates.com</p> \
         <p>(Upcoming) Try these endpoints: /this-week, /last-week, /next-week, /in-two-weeks, /all-weeks</p>"
-
-if __name__ == "__main__":
-    app.run()
-
-# Example request response from quering the TMDb API for a movie given its IMDb ID
-# {
-#   "movie_results": [
-#     {
-#       "vote_average": 8.2,
-#       "overview": "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
-#       "release_date": "1977-05-25",
-#       "adult": false,
-#       "backdrop_path": "/4qCqAdHcNKeAHcK8tJ8wNJZa9cx.jpg",
-#       "vote_count": 15455,
-#       "genre_ids": [
-#         12,
-#         28,
-#         878
-#       ],
-#       "id": 11,
-#       "original_language": "en",
-#       "original_title": "Star Wars",
-#       "poster_path": "/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
-#       "title": "Star Wars",
-#       "video": false,
-#       "popularity": 59.544
-#     }
-#   ],
-#   "person_results": [],
-#   "tv_results": [],
-#   "tv_episode_results": [],
-#   "tv_season_results": []
-# }
 
 """
     An application factory for tethering a database to SQLAlchemy models.
@@ -85,3 +42,72 @@ def create_app():
     db.init_app(app)
 
     return app
+
+"""
+    Perform a fetch request to the TMDb API, gathering information for a film by its IMDb ID.
+    Then place this film, along with its release week and IMDb ID, in the database.
+"""
+async def fetch(session, release_week, imdb_id):
+    url = f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={TMDB_API_KEY}&language=en-US&external_source=imdb_id"
+    async with session.get(url) as response:
+        data = await response.json()
+
+        movie_results = data['movie_results']
+        tv_results = data['tv_results']
+
+        if len(movie_results) != 0:
+            movie = movie_results[0]
+            with app.app_context():
+                try:
+                    db.session.add(Movie(imdb_id, 
+                                        movie['id'], 
+                                        movie['title'], 
+                                        movie['poster_path'], 
+                                        movie['overview'], 
+                                        movie['vote_average'], 
+                                        release_week))
+                    db.session.commit()
+                except Exception as e:
+                    print(f"Error: {e}", flush=True)
+                finally:
+                    db.session.close()
+        elif len(tv_results) !=0:
+            show = tv_results[0]
+            with app.app_context():
+                try:
+                    db.session.add(Movie(imdb_id, 
+                                        show['id'], 
+                                        show['name'], 
+                                        show['poster_path'], 
+                                        show['overview'], 
+                                        show['vote_average'], 
+                                        release_week))
+                    db.session.commit()
+                except Exception as e:
+                    print(f"Error: {e}", flush=True)
+                finally:
+                    db.session.close()
+        else:
+            pass
+
+"""
+    Gather all fetch requests to the TMDb API as tasks to be performed at once.
+    Then perform tasks.
+"""
+async def get_tmdb_data(movies):
+    async with aiohttp.ClientSession() as session:
+        with app.app_context():
+            Movie.query.delete()
+        tasks = [fetch(session, release_week, imdb_id) for release_week, imdb_id in movies]
+        await asyncio.gather(*tasks)
+
+"""
+    Perform a webscrape and organize data into a list of tuples containing the release week and IMDb ID for each movie.
+    Then for each tuple, using asyncio, retrieve all film's TMDb information at once. 
+"""
+def scrape_n_save():
+    movies = [(week['release_week'], movie['imdb_id']) for week in scrape() for movie in week['movies']]
+    asyncio.get_event_loop().run_until_complete(get_tmdb_data(movies))
+
+if __name__ == "__main__":
+    app.run()
